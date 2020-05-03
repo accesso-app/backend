57899f2eafe9da86bd08a7111000f907790845d6
Sergey Sova <s.sova@redmadrobot.com>
2020-05-03 19:05:39 +0300

    feat(public-api): WIP. add awc and call sendgrid
    block_on hangs

--- public-api/src/services/email.rs
+++ public-api/src/services/email.rs
@@ -1,17 +1,129 @@
 use authmenow_public_logic::contracts::{EmailMessage, EmailNotification};
 
 #[derive(Clone)]
-pub struct Email {}
+pub struct Email {
+    /// SendGrid api_key
+    pub api_key: String,
 
-impl Email {
-    pub fn new() -> Self {
-        Self {}
-    }
+    /// Who sends email: no-reply@domain.com
+    pub sender_email: String,
+
+    /// domain of the application, without https:// and any path
+    pub application_host: String,
+
+    /// Confirmation url prefix. Should be concatenated with https:// and application_host
+    pub email_confirm_url_prefix: String,
+    pub email_confirm_template: String,
 }
 
 impl EmailNotification for Email {
     fn send(&mut self, email: String, message: EmailMessage) -> bool {
         println!("EMAIL: send {:?} to {}", message, email);
+
+        match message {
+            EmailMessage::RegisterConfirmation { code } => {
+                futures::executor::block_on(async {
+                    let client = awc::Client::default();
+
+                    let resp = client
+                        .post("https://api.sendgrid.com/v3/mail/send")
+                        .header("Authorization", format!("Bearer {}", self.api_key.clone()))
+                        .send_json(&sg::MailSend {
+                            subject: "Confirm registration at Authmenow".to_owned(),
+                            template_id: self.email_confirm_template.clone(),
+                            from: sg::Sender {
+                                email: self.sender_email.clone(),
+                                name: "Authmenow".to_owned(),
+                            },
+                            personalizations: vec![sg::Personalization {
+                                dynamic_template_data: sg::TemplateData {
+                                    application_host: self.application_host.clone(),
+                                    confirm_registration_url: format!(
+                                        "https://{host}{prefix}{code}",
+                                        host = self.application_host,
+                                        prefix = self.email_confirm_url_prefix,
+                                        code = code
+                                    ),
+                                },
+                                to: vec![sg::Target {
+                                    email: email.clone(),
+                                }],
+                            }],
+                        })
+                        .await;
+
+                    println!("EMAIL REQUEST SEND — {:#?}", resp);
+                });
+
+                // let client = reqwest::blocking::Client::new();
+                // let _ = client
+                //     .post("https://api.sendgrid.com/v3/mail/send")
+                //     .header(reqwest::header::AUTHORIZATION, self.api_key.clone())
+                //     .json(&sg::MailSend {
+                //         subject: "Confirm registration at Authmenow".to_owned(),
+                //         template_id: self.email_confirm_template.clone(),
+                //         from: sg::Sender {
+                //             email: self.sender_email.clone(),
+                //             name: "Authmenow".to_owned(),
+                //         },
+                //         personalizations: vec![sg::Personalization {
+                //             dynamic_template_data: sg::TemplateData {
+                //                 application_host: self.application_host.clone(),
+                //                 confirm_registration_url: format!(
+                //                     "https://{host}{prefix}{code}",
+                //                     host = self.application_host,
+                //                     prefix = self.email_confirm_url_prefix,
+                //                     code = code
+                //                 ),
+                //             },
+                //             to: vec![sg::Target {
+                //                 email: email.clone(),
+                //             }],
+                //         }],
+                //     })
+                //     .send();
+            }
+            _ => {}
+        };
+
         true
+    }
+}
+
+mod sg {
+    use serde::Serialize;
+
+    #[derive(Debug, Serialize)]
+    pub struct MailSend {
+        pub personalizations: Vec<Personalization>,
+        pub from: Sender,
+        pub subject: String,
+        pub template_id: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Personalization {
+        pub to: Vec<Target>,
+        pub dynamic_template_data: TemplateData,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Target {
+        pub email: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct TemplateData {
+        #[serde(rename = "applicationHost")]
+        pub application_host: String,
+
+        #[serde(rename = "confirmRegistrationUrl")]
+        pub confirm_registration_url: String,
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct Sender {
+        pub email: String,
+        pub name: String,
     }
 }
