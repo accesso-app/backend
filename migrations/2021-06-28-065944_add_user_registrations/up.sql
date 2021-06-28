1b2fc84a4618976282521e2bf8c40eafd56432f7
Dmitriy Chikhanov <bosshet@gmail.com>
2021-06-28 10:57:13 +0300

    feat: Add user registration table

---
+++ migrations/2021-06-28-065944_add_user_registrations/up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user_registrations"
+(
+    "id"         uuid      NOT NULL DEFAULT uuid_generate_v4(),
+    "client_id"  uuid      NOT NULL REFERENCES clients (id),
+    "created_at" timestamp NOT NULL DEFAULT NOW(),
+    "user_id"    uuid      NOT NULL REFERENCES users (id)
+);
+
+CREATE INDEX "user_registrations_client" ON "user_registrations" USING BTREE ("client_id");
+CREATE INDEX "user_registrations_user" ON "user_registrations" USING BTREE ("user_id");
